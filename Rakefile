4f414939bc629233c0e284a5169b0fec1273af4a
Loic Nageleisen <loic.nageleisen@datadoghq.com>
2024-11-21 11:53:37 +0100

    Add Ruby 1.8.7-p376, 1.9.3-551, 2.0.0-p648 to centos
    These apparently don't support aarch64 at all.

--- Rakefile
+++ Rakefile
@@ -3,4 +3,8 @@
 # @type self: Rake::TaskLib
 
 # load rake tasks from tasks directory
-Dir.glob(File.join(__dir__ || Dir.pwd, "tasks", "**", "*.rake")) { |f| import f }
+if RUBY_VERSION.start_with?("1.8.")
+  import File.join(File.dirname(__FILE__) || Dir.pwd, "tasks", "test.rake")
+else
+  Dir.glob(File.join(File.dirname(__FILE__) || Dir.pwd, "tasks", "**", "*.rake")) { |f| import f }
+end
--- test/engines/test_encoding.rb
+++ test/engines/test_encoding.rb
@@ -1,10 +1,19 @@
 require "minitest/autorun"
 
-# polyfill for Ruby 2.2 and down, used by minitest
+# polyfill for Ruby 2.2 and down, used by minitest assertions
 unless "".respond_to?(:match?)
-  String.instance_eval do
+  class String
     def match?(other)
       (Regexp === other) ? other.match?(self) : (self == other)
+    end
+  end
+end
+
+# polyfill for Ruby 1.9.3 and down, used by minitest assertions
+unless //.respond_to?(:match?)
+  class Regexp
+    def match?(other)
+      !!match(other)
     end
   end
 end
@@ -15,10 +24,16 @@ class TestEncoding < Minitest::Test
   end
 
   def test_utf8_string
-    assert_equal(Encoding::UTF_8, "".encoding)
+    skip "missing String#encoding" unless "".respond_to?(:encoding)
+
+    expected = (RUBY_VERSION =~ /^(?:1\.8\.|1\.9\.)/) ? Encoding::US_ASCII : Encoding::UTF_8
+
+    assert_equal(expected, "".encoding)
   end
 
   def test_read_utf8
+    skip "missing String#encoding" unless "".respond_to?(:encoding)
+
     contents = File.read("test/fixtures/encoding/utf-8.txt")
 
     assert_equal(Encoding::UTF_8, contents.encoding)
